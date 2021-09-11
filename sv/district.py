00e9c822e16907b572afbdadea337deb425ddd0d
Bryan Chu <thisisbryanchu@gmail.com>
2021-09-10 23:45:19 -0700

    Adds some boilerplate for the sv library
    This revision adds a placeholder function in the sv library so other
    engineers can work on it

--- sv/district.py
+++ sv/district.py
@@ -3,10 +3,9 @@ class District:
         (self.district_id, self.properties, self.last_updated) = row_tuple
 
     def __repr__(self):
-        return ("<District object - id: {user_id}, "
-                "last_updated: {last_updated}, "
-                "last_updated: {last_updated}, "
-                "options: {options}>").format(
+        return ("<District object - id: {district_id}, "
+                "properties: {properties}, "
+                "last_updated: {last_updated}>").format(
                     district_id=self.district_id,
                     properties=self.properties,
                     last_updated=self.last_updated
