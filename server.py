dcc354fdbd51fa894e19ff5ba436cf76b2754d1b
Bryan Chu <thisisbryanchu@gmail.com>
2021-09-11 00:48:58 -0700

    Adds reasonable error for nonexistent district

--- server.py
+++ server.py
@@ -16,12 +16,13 @@ DATABASE = sv.svdb.SVDatabase(db_file)
 def district_info(district_id):
     if DATABASE.district_exists(district_id):
         district = DATABASE.get_district_from_id(district_id)
-
-    return {
-        "id": district_id,
-        "properties": district.properties,
-        "last_updated": district.last_updated,
-    }
+        return {
+            "id": district_id,
+            "properties": district.properties,
+            "last_updated": district.last_updated,
+        }
+    else:
+        return {"error": "No such district {}".format(district_id)}
 
 
 run(host="0.0.0.0", port=8080, debug=True, reloader=True)
